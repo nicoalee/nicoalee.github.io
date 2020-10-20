a887809fb3ff9ff87926a94ff123779658e60792
Nicholas Lee <niconal902@gmail.com>
2020-10-19 23:34:34 -0400

    added https redirect

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/personalWebsite'));
 
+app.use((req, res, next) => {
+    if(req.header("x-forwarded-proto") !== "https") {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+        next()
+    }
+})
+
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/personalWebsite/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/personalWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
